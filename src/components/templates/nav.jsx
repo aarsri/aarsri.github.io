090d858c678dec091a906c3306a34f25db08ebb4
joserys <jose.rys@outlook.com>
2026-08-08 21:52:32 -0400

    hopefully this works

--- src/components/templates/nav.jsx
+++ src/components/templates/nav.jsx
@@ -1,6 +1,8 @@
 import { useLocation } from 'react-router-dom';
 import NavButton from '../modules/navButton';
 
+import cvFile from './src/assets/pdfs/CV_for_website.pdf'
+
 const Navbar = () => {
 	const location = useLocation();
 	const currentPage = location.pathname;
@@ -9,7 +11,7 @@ const Navbar = () => {
 		<nav className='navbar'>
 			<NavButton title="About Me" url="/" icon="fa-solid fa-contact-card" active={currentPage == '/'}></NavButton>
 			<NavButton title="Experience" url="/experience" icon="fa-solid fa-suitcase" active={currentPage == '/experience'}></NavButton>
-			<NavButton title="CV" url="/test.pdf" icon="fa-brands fa-envira" active={false}></NavButton>
+			<NavButton title="CV" url={cvFile} icon="fa-brands fa-envira" active={false}></NavButton>
 		</nav>
 	);
 }
