48126e8400b10b8c303ea22248bfcd1b7e3a65b6
joserys <jose.rys@outlook.com>
2026-08-08 21:28:08 -0400

    attempting things

--- src/components/templates/nav.jsx
+++ src/components/templates/nav.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
 
 	return (
 		<nav className='navbar'>
-				<NavButton title="About Me" url="/" icon="fa-solid fa-contact-card" active={currentPage == '/'}></NavButton>
-				<NavButton title="Experience" url="/experience" icon="fa-solid fa-suitcase" active={currentPage == '/experience'}></NavButton>
-				<NavButton title="CV" url="./src/assets/pdfs/CV_for_website.pdf" icon="fa-brands fa-envira" active={false}></NavButton>
+			<NavButton title="About Me" url="/" icon="fa-solid fa-contact-card" active={currentPage == '/'}></NavButton>
+			<NavButton title="Experience" url="/experience" icon="fa-solid fa-suitcase" active={currentPage == '/experience'}></NavButton>
+			<NavButton title="CV" url="./CV_for_website.pdf" icon="fa-brands fa-envira" active={false}></NavButton>
 		</nav>
 	);
 }
